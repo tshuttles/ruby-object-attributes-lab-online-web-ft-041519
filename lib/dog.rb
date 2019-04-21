36b9d70552cb317f05a32426ade0f4330172e4fd
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-21 23:47:15 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name 
+    @name = name 
+  end 
+  
+  def name=(name)
+    @name
+  end 
+  
+  def breed 
+    @breed = breed 
+  end 
+  
+  def breed=(breed)
+    @breed 
+  end 
+end 
