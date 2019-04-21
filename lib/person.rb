7d7249c86d66bdcd68e02aa919c55f110ba3524c
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-21 23:49:23 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+  def name 
+    @name 
+  end 
+  
+  def name=(new_name)
+    @new_name = new_name
+  end 
+  
+  def breed 
+    @breed 
+  end 
+  
+  def breed=(new_breed)
+    @new_breed = new_breed
+  end 
+end 
